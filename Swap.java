3983fbba8fef7c9967b3e4f6fd09eaf5d1bc7086
91996 <vickramankeerthan123@gmail.com>
2022-10-20 21:15:48 +0530

    first commit

---
+++ Swap.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Swap {
+    public static void main(String[] args) {
+        int x, y, temp;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of X and Y");
+        x = sc.nextInt();
+        y = sc.nextInt();
+        System.out.println("before swapping numbers: "+x +"  "+ y);
+        temp = x;
+        x = y;
+        y = temp;
+        System.out.println("After swapping: "+x +"   " + y);
+        System.out.println( );
+    }
+}
